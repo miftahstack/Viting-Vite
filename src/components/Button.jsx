1b747798ea41cd3e9eacf6b4bbe65ff36f4e76f1
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-06-19 04:07:06 +0600

    Tailwind Install

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+
+
+const Button = () => {
+  return (
+    <>
+    <button className="bg-blue-500 cursor-pointer text-white p-4 rounded hover:bg-blue-700 transition duration-300">
+      Click Me
+    </button>
+    </>
+  )
+}
+
+export default Button
